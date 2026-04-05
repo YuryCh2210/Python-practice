833c00cde3dbb0834db4a812084c4e3ed0cb01aa
YuryCh2210 <Hernandez.lab@hotmail.com>
2026-04-05 17:57:47 -0600

    calcular_edad.py

---
+++ calcular_edad.py
@@ -0,0 +1,6 @@
+nombre = input ("¿Como te llamas?")
+edad = int(input("¿Cuantos años tienes?"))
+if edad >= 18:
+  print(nombre + ", eres mayor de edad")
+else:
+  print(nombre + ", eres menor de edad")
