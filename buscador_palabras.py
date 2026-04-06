07cf63d3ae2294ee26bb9459123fdd69b13288b2
YuryCh2210 <Hernandez.lab@hotmail.com>
2026-04-05 18:17:20 -0600

    buscador_palabras.py

---
+++ buscador_palabras.py
@@ -0,0 +1,6 @@
+texto = input ("Escribe una frase:")
+palabra = input ("¿Cual palabra wuieres buscar?")
+if palabra in texto:
+  print("La palabra sí está en el texto")
+else:
+  print("La palabra no está en el texto")
